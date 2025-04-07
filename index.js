b4272210e78e1428800c1c03b6ecfe485c8a2a2c
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2025-04-07 01:35:23 -0900

    modified microservice

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -18,10 +17,39 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
+app.get("/api/",(req,res)=>{
+  const d = new Date()
+  utc =d.toUTCString()
+  unix = d.valueOf()
+  res.json({"unix":unix,"utc":utc})
+})
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = req.params.date
+
+    if ( date == ""){
+      res.json({"error":"Invalid Date"})  
+    }else{
+      if (isNaN(date)){
+
+        const d = new Date(date)
+        if (d == "Invalid Date"){
+          res.json({"error":"Invalid Date"})
+        }else{
+          utc =d.toUTCString()
+          unix = d.valueOf()
+          res.json({"unix":unix,"utc":utc})
+        }
+        } 
+      else{
+        d = new Date(date *1000)
+        utc =d.toUTCString()
+        unix = d.valueOf()
+        res.json({"unix":unix,"utc":utc})
+      }   
+    }
+ 
+
 });
 
 
